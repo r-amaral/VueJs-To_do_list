a73bf525ac43b56725da573bd1f16506f958361f
Ruan Oliveira <ruan.amaral@compasso.com.br>
2024-12-11 17:14:12 -0300

    feat: creating Logo component

---
+++ src/components/Logo/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Logo from "../../assets/bextvc_logo-removebg-preview_1.png";
+</script>
+
+<template>
+  <div class="flex items-center ml-[-42px]">
+    <img :src="Logo" alt="Logo Bext" class="w-[120px] h-[100px]" />
+    <span class="text-[78px] font-bold text-white">bext</span>
+  </div>
+</template>

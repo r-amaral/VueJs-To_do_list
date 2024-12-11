874b7b779190fa8233e09f4c5eeec2039193fa5e
Ruan Oliveira <ruan.amaral@compasso.com.br>
2024-12-11 17:15:00 -0300

    feat: developing initial structure for login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,66 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive } from "vue";
+import Logo from "../components/Logo/index.vue";
+import Button from "../components/Button/index.vue";
+import FieldText from "../components/FieldText/index.vue";
 
-defineProps<{ msg: string }>();
+const user = reactive({
+  email: "",
+  password: "",
+});
 
-const count = ref(0);
+const login = () => {
+  console.log(user.email, user.password);
+};
 </script>
 
 <template>
-  <main>
-    <section>
-      <h1>{{ msg }}</h1>
-      <h1 class="text-3xl font-bold bg-zinc-950">Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
-      </form>
+  <main class="grid grid-cols-2 min-h-[100vh] bg-white">
+    <section class="flex justify-center items-center h-[100%]">
+      <div
+        class="flex justify-center h-[100%] items-center flex-col w-full"
+      >
+        <div class="mb-[100px] flex flex-col items-center">
+          <h1 class="text-7xl font-bold text-blue-800">Welcome</h1>
+          <span class="text-black font-medium"
+            >Organize your life. Organize your mind.</span
+          >
+        </div>
+        <form
+          class="flex justify-center items-center flex-col w-full gap-5"
+          @submit.prevent="login"
+        >
+          <FieldText
+            id="email"
+            type="text"
+            placeholder="Type your email"
+            label="Email"
+            v-model="user.email"
+          />
+          <FieldText
+            id="password"
+            type="password"
+            placeholder="Type your password"
+            label="Password"
+            v-model="user.password"
+          />
+
+          <span>
+            If you do not have an account,
+            <router-link
+              class="text-blue-800 font-bold"
+              to="/register"
+              >Click here</router-link
+            ></span
+          >
+          <Button text="Login" />
+        </form>
+      </div>
     </section>
-    <section>
-      <img src="" alt="" />
+    <section
+      class="flex shadow-xl shadow-blue-800/100 items-center justify-center h-[100%] p-[100px] rounded-tl-[50px] rounded-bl-[50px] bg-[linear-gradient(to_right,_rgb(23,_45,_171),_rgb(31,_35,_66))]"
+    >
+      <Logo />
     </section>
   </main>
 </template>
-
-<style scoped></style>

4edd48d7b8f6085376deaf382ab041a772ed883b
Ruan Oliveira <ruan.amaral@compasso.com.br>
2024-12-12 11:09:12 -0300

    feat: adding section in logo component

--- src/components/Logo/index.vue
+++ src/components/Logo/index.vue
@@ -3,8 +3,12 @@ import Logo from "../../assets/bextvc_logo-removebg-preview_1.png";
 </script>
 
 <template>
-  <div class="flex items-center ml-[-42px]">
-    <img :src="Logo" alt="Logo Bext" class="w-[120px] h-[100px]" />
-    <span class="text-[78px] font-bold text-white">bext</span>
-  </div>
+  <section
+    class="flex shadow-xl shadow-blue-800/100 items-center justify-center h-[100%] p-[100px] rounded-tl-[50px] rounded-bl-[50px] bg-[linear-gradient(to_right,_rgb(23,_45,_171),_rgb(31,_35,_66))]"
+  >
+    <div class="flex items-center ml-[-42px]">
+      <img :src="Logo" alt="Logo Bext" class="w-[120px] h-[100px]" />
+      <span class="text-[78px] font-bold text-white">bext</span>
+    </div>
+  </section>
 </template>

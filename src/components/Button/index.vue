0371ca767951c4aad5f608818a82845eaccabc10
Ruan Oliveira <ruan.amaral@compasso.com.br>
2024-12-12 11:06:59 -0300

    feat: developing initial structure for button component

---
+++ src/components/Button/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ButtonProps } from "../../types";
+
+defineProps<ButtonProps>();
+
+const emit = defineEmits(["click"]);
+</script>
+
+<template>
+  <button
+    class="w-full p-4 max-w-[375px] bg-blue-800 text-white rounded-lg shadow-md shadow-blue-800/50"
+    @click="$emit('click')"
+  >
+    {{ text }}
+  </button>
+</template>

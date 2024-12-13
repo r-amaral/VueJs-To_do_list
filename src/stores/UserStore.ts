ba07b8d53ad69b668c6678424ac165e0dec2f7b6
Ruan Oliveira <ruan.amaral@compasso.com.br>
2024-12-13 06:04:13 -0300

    feat: creating stores

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,28 +1,22 @@
-import { defineStore } from "pinia";
-import { UserTypes } from "../types";
+import { defineStore } from 'pinia';
+import { UserTypes } from '../types';
 
-export const useUserStore = defineStore("user", {
-  state() {
-    return {
-      user: {
-        email: "",
-        fullName: "",
-        id: "",
-        password: "",
-        tasks: [],
-      } as UserTypes,
-    };
-  },
+export const useUserStore = defineStore('user', {
+   state() {
+      return {
+         user: {
+            email: '',
+            fullName: '',
+            id: '',
+            password: '',
+            tasks: [],
+         } as UserTypes,
+      };
+   },
 
-  actions: {
-    setUser(user: UserTypes) {
-      this.user = user;
-    },
-  },
-
-  getters: {
-    showUser(): string {
-      return "The user is: " + this.user;
-    },
-  },
+   actions: {
+      setUser(userData: UserTypes) {
+         this.user = userData;
+      },
+   },
 });

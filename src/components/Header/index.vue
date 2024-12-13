bd7147eef2275097de352f62933d0896e8ec8d0c
Ruan Oliveira <ruan.amaral@compasso.com.br>
2024-12-13 06:14:01 -0300

    feat: adding sign out button

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,25 +1,40 @@
 <script setup lang="ts">
-import Logo from "../../assets/bextLogo.png";
-import User from "../../assets/user.svg";
+import Logo from '../../assets/bextLogo.png';
+import User from '../../assets/user.svg';
+import { useRouter } from 'vue-router';
+import { useUserStore } from '../../stores/UserStore';
+
+const router = useRouter();
+const { user } = useUserStore();
+
+const loggout = () => {
+   localStorage.removeItem('logged');
+   router.push('/');
+};
 </script>
 
 <template>
-  <header class="flex justify-between p-[30px]">
-    <div class="flex items-center">
-      <img :src="Logo" alt="" class="w-[100px] h-[100px]" />
-      <h3 class="text-blue-800 font-bold text-[32px]">bext</h3>
-    </div>
-    <div class="flex flex-col items-center">
-      <h2 class="font-bold text-[32px] mb-2">Welcome, Ruan!</h2>
-      <span class="font-medium text-[18px] text-black"
-        >Organize your life. Organize your mind.</span
-      >
-    </div>
-    <div class="flex gap-[13px] items-center h-[90px]">
-      <span>Ruan</span>
-      <button>
-        <img :src="User" alt="Icon user" />
-      </button>
-    </div>
-  </header>
+   <header class="flex justify-between p-[30px]">
+      <div class="flex items-center">
+         <img :src="Logo" alt="" class="w-[100px] h-[100px]" />
+         <h3 class="text-blue-800 font-bold text-[32px]">bext</h3>
+      </div>
+      <div class="flex flex-col items-center">
+         <h2 class="font-bold text-[32px] mb-2">Welcome, {{ user.fullName.split(' ')[0] }}!</h2>
+         <span class="font-medium text-[18px] text-black"
+            >Organize your life. Organize your mind.</span
+         >
+      </div>
+      <div class="flex gap-[13px] items-center h-[90px]">
+         <span>{{ user.fullName.split(' ')[0] }}</span>
+         <span
+            class="absolute top-[5px] right-[15px] font-bold text-blue-800 cursor-pointer"
+            @click="loggout"
+            >Sign out</span
+         >
+         <button>
+            <img :src="User" alt="Icon user" />
+         </button>
+      </div>
+   </header>
 </template>

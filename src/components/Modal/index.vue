ddc1bf5cbebbd6afa13ef9ab8b8bb565e42dc796
Ruan Oliveira <ruan.amaral@compasso.com.br>
2024-12-13 06:01:25 -0300

    feat: refact lint

--- src/components/Modal/index.vue
+++ src/components/Modal/index.vue
@@ -1,39 +1,36 @@
 <script setup lang="ts">
-import XMark from "../../assets/x-mark.webp";
-import { defineProps, defineEmits } from "vue";
+import XMark from '../../assets/x-mark.webp';
+import { defineProps, defineEmits } from 'vue';
 
-defineProps<{ open: boolean }>();
-const emit = defineEmits(["close"]);
+const props = defineProps<{ open: boolean }>();
+const emit = defineEmits(['close']);
 
 const onClose = () => {
-  emit("close");
+   emit('close');
 };
 </script>
 
 <template>
-  <transition
-    enter-active-class="transition-opacity duration-500"
-    enter-from-class="opacity-0"
-    enter-to-class="opacity-100"
-    leave-active-class="transition-opacity duration-500"
-    leave-from-class="opacity-100"
-    leave-to-class="opacity-0"
-  >
-    <div
-      v-if="open"
-      class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center w-[100vw] h-[100vh]"
-    >
+   <transition
+      enter-active-class="transition-opacity duration-300"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition-opacity duration-300"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+   >
       <div
-        class="min-w-[10%] min-h-[10%] max-w-[80%] max-h-[90%] bg-white relative overflow-auto"
+         v-if="props.open"
+         class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center w-[100vw] h-[100vh]"
       >
-        <button
-          @click="onClose"
-          class="w-[30px] h-[30px] absolute top-[10px] right-[20px]"
-        >
-          <img :src="XMark" alt="icon close modal" />
-        </button>
-        <slot></slot>
+         <div
+            class="min-w-[10%] min-h-[10%] max-w-[80%] max-h-[90%] rounded-xl bg-white relative overflow-auto"
+         >
+            <button @click="onClose" class="w-[30px] h-[30px] absolute top-[10px] right-[20px]">
+               <img :src="XMark" alt="icon close modal" />
+            </button>
+            <slot></slot>
+         </div>
       </div>
-    </div>
-  </transition>
+   </transition>
 </template>
--- src/components/PriorityCard/index.vue
+++ src/components/PriorityCard/index.vue
@@ -1,35 +1,36 @@
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { defineProps } from 'vue';
 
-const props = defineProps<{
-  priority: string;
-  isSelected: boolean;
-}>();
-const { priority, isSelected } = props;
+const props = defineProps<{ priority: string; isSelected: boolean }>();
 
 const getCardColor = (priority: string) => {
-  switch (priority) {
-    case "High":
-      return "bg-red-500 border-red-500 ";
-    case "Medium":
-      return "bg-yellow-500 border-yellow-500 ";
-    case "Low":
-      return "bg-green-500 border-green-500 ";
-    default:
-      return null;
-  }
+   switch (priority) {
+      case 'High':
+         return 'bg-red-500 border-red-500';
+      case 'Medium':
+         return 'bg-yellow-500 border-yellow-500';
+      case 'Low':
+         return 'bg-green-500 border-green-500';
+      default:
+         return null;
+   }
 };
-const cardColor = getCardColor(priority);
+
+const cardColor = getCardColor(props.priority);
 </script>
 
 <template>
-  <li
-    class="cursor-pointer flex flex-col items-center w-full max-w-[500px] group"
-  >
-    <span
-      class="w-[45px] h-[45px] rounded-full mb-2 border transition-all duration-300 ease-in-out group-hover:bg-white"
-      :class="[isSelected ? 'bg-white' : '', cardColor]"
-    ></span>
-    <span>{{ priority }}</span>
-  </li>
+   <li class="cursor-pointer flex flex-col items-center w-full max-w-[500px] group">
+      <span
+         :class="[cardColor, { selectedStyle: isSelected }]"
+         class="w-[45px] h-[45px] rounded-full mb-2 border transition-all duration-300 ease-in-out group-hover:bg-white"
+      ></span>
+      <span>{{ priority }}</span>
+   </li>
 </template>
+
+<style scoped>
+.selectedStyle {
+   background-color: rgb(255 255 255);
+}
+</style>

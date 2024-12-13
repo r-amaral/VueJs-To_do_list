f21b3a046802f872ef4a56cfad2529daacca1f85
Ruan Oliveira <ruan.amaral@compasso.com.br>
2024-12-13 06:05:24 -0300

    feat: developing dashboard page

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,125 +1,68 @@
 <script setup lang="ts">
-import Header from "../../components/Header/index.vue";
-import { useUserStore } from "../../stores/UserStore.ts";
-import Card from "../../components/Card/index.vue";
-import Button from "../../components/Button/index.vue";
-import PriorityCard from "../../components/PriorityCard/index.vue";
-import Modal from "../../components/Modal/index.vue";
-import FieldText from "../../components/FieldText/index.vue";
-import { reactive, ref } from "vue";
-import { CategoriesTypes, PriorityTypes } from "../../types/index.ts";
+import { ref, computed } from 'vue';
+import Filter from './Filter/Filter.vue';
+import { CardProps } from '../../types/index.ts';
+import TaskModal from './TaskModal/TaskModal.vue';
+import Card from '../../components/Card/index.vue';
+import Header from '../../components/Header/index.vue';
+import { useUserStore } from '../../stores/UserStore.ts';
+import { useFilterStore } from '../../stores/FilterStore';
 
 const open = ref(false);
+const openFilter = ref(false);
 const userStore = useUserStore();
+const filterStore = useFilterStore();
 
-const newTask = reactive({
-  title: "",
-  date: "",
-  priority: "",
-  description: "",
-  category: "",
+const selectedTask = ref<CardProps | null>(null);
+
+const setModalState = (isOpen: boolean, task: CardProps | null = null) => {
+   open.value = isOpen;
+   selectedTask.value = task;
+};
+
+const openModal = () => setModalState(true);
+const openEditModal = (currentTask: CardProps) => setModalState(true, currentTask);
+const closeModal = () => setModalState(false);
+const onOpenFilter = (status: boolean) => (openFilter.value = status);
+
+const filteredTasks = computed(() => {
+   return userStore.user.tasks?.filter(task => {
+      const matchesCategory =
+         filterStore.category.length === 0 || filterStore.category.includes(task.category);
+      const matchesPriority = !filterStore.priority || filterStore.priority === task.priority;
+      return matchesCategory && matchesPriority;
+   });
 });
-
-const openModal = (state: boolean) => (open.value = state);
 </script>
 
 <template>
-  <Header />
-
-  <main class="w-full flex justify-center">
-    <section class="w-[70%]">
-      <div class="w-full mb-[10px] flex justify-between">
-        <h3>Tasks</h3>
-        <Button class="w-[135px] p-[8px]" @click="openModal(true)">
-          New task</Button
-        >
-      </div>
-      <ul
-        v-if="userStore.user.tasks?.length"
-        class="flex p-[30px] flex-col gap-[20px] w-full items-center border rounded-lg border-blue-800"
-      >
-        <Card
-          v-for="(task, index) in userStore.user.tasks"
-          :key="index"
-          :date="task.date"
-          :title="task.title"
-          :priority="task.priority"
-        />
-      </ul>
-    </section>
-
-    <Modal :open="open" @close="openModal(false)">
-      <div class="flex flex-col p-[30px]">
-        <h3 class="font-bold text-[40px] text-center mb-[20px]">
-          New Task
-        </h3>
-        <div class="flex flex-col grid-cols-2 gap-[10px]">
-          <FieldText
-            id="title"
-            type="text"
-            placeholder="Type your title"
-            label="Title"
-            v-model="newTask.title"
-            class="max-w-none"
-          />
-          <FieldText
-            id="description"
-            type="textfield"
-            placeholder="Type your description"
-            label="Description"
-            v-model="newTask.description"
-            :isTextArea="true"
-          />
-        </div>
-
-        <div class="mt-[20px]">
-          <h4>Choose the categories</h4>
-          <div class="flex gap-[20px] mt-[10px]">
-            <Button
-              v-for="(categorie, i) in CategoriesTypes"
-              :key="i"
-              class="min-w-[100px] p-[10px]"
-              :class="
-                newTask.category === categorie &&
-                'text-blue-700 bg-white'
-              "
-              @click="newTask.category = categorie"
-              >{{ categorie }}</Button
-            >
-          </div>
-
-          <div class="mt-[20px]">
-            <h4>DeadLine:</h4>
-            <FieldText
-              id="date"
-              type="date"
-              placeholder="Type your description"
-              v-model="newTask.date"
+   <Header />
+   <main class="w-full flex justify-center">
+      <section class="w-[70%]">
+         <div class="w-full flex justify-between">
+            <h3 class="text-[32px] font-bold">My Board</h3>
+            <Filter :openModal="openModal" :openFilter="openFilter" :onOpenFilter="onOpenFilter" />
+         </div>
+         <ul
+            v-if="filteredTasks?.length ?? 0"
+            class="flex p-[30px] flex-col gap-[20px] w-full items-center"
+         >
+            <Card
+               v-for="(task, index) in filteredTasks"
+               :key="index"
+               :date="task.date"
+               :title="task.title"
+               :priority="task.priority"
+               @click="openEditModal(task)"
             />
-          </div>
+         </ul>
+      </section>
 
-          <div class="mt-[20px]">
-            <h4>What's the priority?</h4>
-            <ul class="flex gap-[20px] mt-[15px]">
-              <PriorityCard
-                v-for="(priority, i) in PriorityTypes"
-                :key="i"
-                :priority="priority"
-                :isSelected="newTask.priority === priority"
-                @click="newTask.priority = priority"
-              />
-            </ul>
-          </div>
-
-          {{ console.log(newTask.priority) }}
-          <div class="mt-[20px] flex gap-[20px]">
-            <Button class="p-[10px]" @click="openModal(false)"
-              >Cancel</Button
-            >
-            <Button class="p-[10px]">Create</Button>
-          </div>
-        </div>
-      </div>
-    </Modal>
-  </main>
+      <TaskModal
+         :open="open"
+         @close="closeModal"
+         :openModal="openModal"
+         :selectedTask="selectedTask"
+      />
+   </main>
 </template>
